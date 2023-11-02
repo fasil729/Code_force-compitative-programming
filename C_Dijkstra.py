173206ffbf82452e99dd8d2bf6beb0bc2f71f2b8
fasika <fasikafikadu38@gmail.com>
2023-11-02 15:35:48 +0300

    add C-Dijkstr

--- C_Dijkstra.py
+++ C_Dijkstra.py
@@ -4,22 +4,36 @@ import heapq
 n, m = map(int, input().split())
 graph = defaultdict(set)
 for _ in range(m):
-    u, v = map(int, input().split())
-    graph[u].add(v)
-    graph[v].add(u)
-    
-def train(start):
+    u, v, w = map(int, input().split())
+    graph[u].add((v, w))
+    graph[v].add((u, w))
+
+def dijkstra(start):
     heap = [(0, start)]
-    visited = set()
+    ans = {key:(float("inf"), -1) for key in range(1, n + 1)}
+    ans[1] = (0, -1)
     while heap:
         dist, node = heapq.heappop(heap)
         if node == n:
-            return dist
-        visited.add(node)
+            return ans
         
-        for neigh in graph[node]:
-            if neigh in visited:
-                continue
-            new_dist = dist + 1
-            heapq.heappush(heap, (new_dist, neigh))
+        for neigh, cost in graph[node]:
+            new_dist = dist + cost
+            if ans[neigh][0] > new_dist:
+                ans[neigh] = (new_dist, node)
+                heapq.heappush(heap, (new_dist, neigh))
+            
+            
     return -1
+dist = dijkstra(1)
+if dist == -1:
+    print(-1)
+else:
+    ans = [str(n)]
+    prev = n
+    while dist[prev][1] != 1:
+    
+        ans.append(str(dist[prev][1]))
+        prev = dist[prev][1]
+    ans.append("1")
+    print(" ".join(ans[::-1]))
